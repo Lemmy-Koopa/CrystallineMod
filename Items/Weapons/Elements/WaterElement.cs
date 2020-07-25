f162029d2b3da665b041f8513eefb24eb3307a27
Lemmy-Koopa <56778754+Lemmy-Koopa@users.noreply.github.com>
2020-07-25 16:13:16 -0400

    elements

---
+++ Items/Weapons/Elements/WaterElement.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CrystallineMod.Items.Elements
+{
+    public class WaterElement : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Used to Craft Water Items.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 999;
+            item.value = 100;
+            item.rare = ItemRarityID.Blue;
+            
+        }
+
+       
+    }
+}
